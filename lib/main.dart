14eef013e377847e9a734ea639b4022971b62097
Notebook Exatel <tec.exatel@gmail.com>
2023-05-29 17:57:22 -0300

    color green

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      color: Colors.green,
     );
   }
 }
